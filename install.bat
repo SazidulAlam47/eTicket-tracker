fa90587baf5efddb0885b8104838cd32897c86a0
Sazidul Alam <sazidulalam20@gmail.com>
2025-03-18 14:13:10 +0600

    added puppeteer browser install command in install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,21 @@
 @echo off
+
+echo Installing Puppeteer Chrome browser...
+echo Y | npx puppeteer browsers install chrome
+
+if %errorlevel% neq 0 (
+    echo Failed to install Puppeteer. Exiting...
+    exit /b %errorlevel%
+)
+
+echo Puppeteer installation completed successfully.
+
+echo Setting up the backend...
 start cmd /k "cd eticket-server && npm install && exit"
+
+echo Setting up the frontend...
 start cmd /k "cd eticket-client && npm install && npm run build && exit"
+
+echo All setup processes have been started.
+
+exit
